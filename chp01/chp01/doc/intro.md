e90af4c5b7ad4f38c1e575e8e53b9c57a72f866e
mr-gl4ss <mcglass84@gmail.com>
2019-01-01 18:51:02 -0800

    adding project for chapter 1

---
+++ chp01/chp01/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to chp01
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ chp01/chp01/test/chp01/core_test.clj
@@ -0,0 +1,7 @@
+(ns chp01.core-test
+  (:require [clojure.test :refer :all]
+            [chp01.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
